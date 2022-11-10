216c8b85e24aac653b743801787750b61e974bd5
Fatur <frahmann11@gmail.com>
2022-11-11 01:12:15 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app   = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'asd82a'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
